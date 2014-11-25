05504e7e81e2f2abb7526eecca4b56b0f87afdb9
Morten Nobel-Jørgensen <morten@nobel-joergensen.com>
2014-11-25 14:31:50 +0100

    Add license and GH

--- navigator.js
+++ navigator.js
@@ -68,6 +68,7 @@ function hasWebGLSupport(){
 }
 
 function insertPageTitle(){
+    document.write('<a href="http://mortennobel.github.io/WebGLForOpenGLProgrammers/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"></a>');
     document.write("<h1>WebGL for OpenGL Programmers</h1>");
     if (!hasWebGLSupport()) {
         document.write("<p class='warning'>Your browser does not seem to support WebGL. Try upgrading your webbrowser and/or your graphics card driver.</p>");
